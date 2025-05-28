4ce93c1f1390ddf17e9a88cb71d73ecf09e431b7
Roman <roman-meinl@ukr.net>
2025-05-28 15:06:27 -0200

    reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { useDispatch, useSelector } from "react-redux";
-import { fetchData } from "./store/dataSlice";
+
 
 
 function App() {
-  const allData = useSelector((state) => state.data.posts);
-  const dispatch = useDispatch()
-  const fetchPosts = () =>{
-    dispatch(fetchData())
-  }
-  const showPosts = () =>{
-    console.log(allData);
-    
-  }
-
-  
+ 
   return (
     <>
-    <div>
-     <button style={{background:"#333",color:"white", margin:"5px"}} onClick={fetchPosts}>добавить пост</button>
-     <button style={{background:"#334", color:"white",margin:"5px"}} onClick={showPosts}>показать пост</button>
+    <div style={{marginTop:"50px"}}>
+      
+     <button>добавить пост</button>
+     <button>показать пост</button>
     </div>
     </>
   )
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { dataReducer } from "./dataSlice";
-
- export const store = configureStore({
-    reducer:{
-        data: dataReducer
-    }
- })
