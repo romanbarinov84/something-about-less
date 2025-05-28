8d373baee34a744667e3d7a7cd0799845149376a
Roman <roman-meinl@ukr.net>
2025-05-28 16:25:01 -0200

    createAsyncTunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux";
+import { fetchData } from "./store/dataSlice";
 
 
 function App() {
+  const allData = useSelector((state) => state.data.posts);
+  const dispatch = useDispatch();
+
+  const fetchPosts = () => {
+    dispatch(fetchData())
+  }
+  const showPosts = () => {
+    console.log(allData);
+    
+  }
  
   return (
     <>
     <div style={{marginTop:"50px"}}>
       
-     <button>добавить пост</button>
-     <button>показать пост</button>
+     <button onClick={fetchPosts}>добавить пост</button>
+     <button onClick={showPosts}>показать пост</button>
     </div>
     </>
   )
---
+++ src/store/dataSlice.js
@@ -0,0 +1,51 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+    posts: [],
+    isLoading:false,
+    error: null,
+};
+
+ const dataSlice = createSlice({
+    name: "allData",
+    initialState,
+    reducer:{},
+    extraReducers: (builder) => {
+    builder
+    .addCase(
+      fetchData.fulfilled,
+      (state, action) => {
+        state.isLoading = false
+        state.error = null
+        state.posts = action.payload
+    })
+    .addCase(
+      fetchData.pending,
+      (state, action) => {
+        state.posts = action.payload
+        state.error = null
+        state.isLoading = true;
+    })
+      
+    .addCase(
+      fetchData.rejected,
+      (state, action) => {
+        state.posts = action.payload
+        state.isLoading = false;
+        state.error = action.error.message;
+    });
+      
+      }
+    
+  },
+)
+
+export const fetchData = createAsyncThunk("allData/fetchData", async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await response.json()
+    return data
+    
+})
+
+
+export  const dataReducer = dataSlice.reducer;
